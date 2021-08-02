02130d55b348170de19cd089677fc31b9fb5fc8c
Dan Anstis <dan@bsod.co.nz>
2021-08-02 12:16:06 +1000

    fix: panic on friend search (#12)

--- openxbl/friends.go
+++ openxbl/friends.go
@@ -27,9 +27,12 @@ func (s *FriendsService) Search(gt string) (User, error) {
 	}
 
 	var friendSeachResp *FriendSeachResp
-	_, err = s.client.Do(req, &friendSeachResp)
+	resp, err := s.client.Do(req, &friendSeachResp)
 	if err != nil {
 		return User{}, err
+	}
+	if friendSeachResp == nil {
+		return User{}, fmt.Errorf("something went wrong, try again. Details(%s)", resp.Status)
 	}
 
 	if len(friendSeachResp.ProfileUsers) < 1 {
